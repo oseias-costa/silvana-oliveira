a6667e6a7ff2213b33c04137f4c1d57c6450c150
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-05-04 09:46:22 -0300

    update: texts about

--- app/components/forWho/_data.tsx
+++ app/components/forWho/_data.tsx
@@ -1,9 +1,9 @@
 
 export const data = [
-    {
-        id: 1,
-        text: 'Sempre sonhou em pintar mas nunca teve tempo'
-    },
+    // {
+    //     id: 1,
+    //     text: 'Sempre sonhou em pintar mas nunca teve tempo'
+    // },
     {
         id: 2,
         text: 'Quer ocupar o tempo livre e ocupar a mente'
@@ -12,10 +12,10 @@ export const data = [
         id: 3,
         text: 'Quer aumentar a auto estima e viver mais feliz'
     },
-    {
-        id: 4,
-        text: 'Quer presentear com algo bem especial feito por ti'
-    },
+    // {
+    //     id: 4,
+    //     text: 'Quer presentear com algo bem especial feito por ti'
+    // },
     {
         id: 5,
         text: 'Quer decorar a casa fazendo seus próprios quadros'
